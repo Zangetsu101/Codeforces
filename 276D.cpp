8034353bb3a76f9e489b9c5cc051c72be0ae9e40
Zangetsu101 <haidertameem@gmail.com>
2017-07-14 21:22:22 +0600

    Create 276D.cpp

---
+++ 276D.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+#define endl "\n"
+#define sf(a) scanf("%d",&a)
+#define sff(a,b) scanf("%d %d",&a,&b)
+#define sfff(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pos(ll a)
+{
+    int res=-1;
+    while(a)
+        res++,a>>=1;
+    return max(0,res);
+}
+
+ll solve(ll l,ll r)
+{
+    if(r-l<=1)
+        return r^l;
+    int a=pos(l),b=pos(r);
+    ll mask=~(1LL<<a);
+    if(a==b)
+        return solve(l&mask,r&mask);
+    return (1LL<<b+1)-1;
+}
+
+int main()
+{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+    ll l,r;
+    cin>>l>>r;
+    cout<<solve(l,r)<<endl;
+    return 0;
+}
