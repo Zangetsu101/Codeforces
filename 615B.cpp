1cab316f7a1f17841565882f3b38dd4272c94ea1
Zangetsu101 <haidertameem@gmail.com>
2017-07-14 21:22:47 +0600

    Create 615B.cpp

---
+++ 615B.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+#define endl "\n"
+#define sf(a) scanf("%d",&a)
+#define sff(a,b) scanf("%d %d",&a,&b)
+#define sfff(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+vector< vi > adj(N);
+int maxLen[N];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    for(int i=0;i<m;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for(int i=1;i<=n;i++)
+        maxLen[i]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<adj[i].size();j++)
+        {
+            if(adj[i][j]>i)
+                maxLen[adj[i][j]]=max(maxLen[adj[i][j]],maxLen[i]+1);
+        }
+    }
+    ll ans=0;
+    for(int i=1;i<=n;i++)
+        ans=max(ans,1LL*maxLen[i]*adj[i].size());
+    cout<<ans<<endl;
+    return 0;
+}
