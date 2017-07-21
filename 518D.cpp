fd1edd3c6a4fabb0f1b2b79c3c41c8aba134fb68
Zangetsu101 <haidertameem@gmail.com>
2017-07-21 12:08:37 +0600

    Create 518D.cpp

---
+++ 518D.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+#define endl "\n"
+#define sf(a) scanf("%d",&a)
+#define sff(a,b) scanf("%d %d",&a,&b)
+#define sfff(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 2001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+double dp[N][N];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+    int n,t;
+    double p;
+    cin>>n>>p>>t;
+    dp[0][0]=1;
+    for(int i=1;i<=t;i++)
+    {
+        for(int j=0;j<=min(i,n);j++)
+        {
+            if(j>=1)
+                dp[i][j]+=dp[i-1][j-1]*p;
+            if(j!=n)
+                dp[i][j]+=dp[i-1][j]*(1-p);
+            else
+                dp[i][j]+=dp[i-1][j];
+        }
+    }
+    double ans=0;
+    for(int i=1;i<=n;i++)
+        ans+=i*dp[t][i];
+    cout<<fixed<<setprecision(8)<<ans<<endl;
+    return 0;
+}
