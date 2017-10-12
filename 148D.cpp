f37279bddbed9161fd9a7fd2fa88fb77a19304ef
Zangetsu101 <haidertameem@gmail.com>
2017-10-12 22:13:21 +0600

    Create 148D.cpp

---
+++ 148D.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+#define endl "\n"
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b)
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 1001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+double dp[N][N][2];
+bool flag[N][N][2];
+
+double solve(int w,int b,double p,bool f)
+{
+    double ans=0;
+    if(!w&&!b||!w||!b&!f||w<0||b<0)
+        return 0;
+    if(!b&f)
+        return p;
+    if(flag[w][b][f])
+        return dp[w][b][f];
+    flag[w][b][f]=true;
+    if(f)
+    {
+        ans+=p*w/(w+b);
+        ans+=solve(w,b-1,p*b/(w+b),false);
+        return dp[w][b][f]=ans;
+    }
+    else
+        return dp[w][b][f]=solve(w-1,b-1,1.0*w/(w+b-1)*p*b/(w+b),true)+solve(w,b-2,1.0*(b-1)/(w+b-1)*p*b/(w+b),true);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+    int w,b;
+    cin>>w>>b;
+    cout<<fixed<<setprecision(9)<<solve(w,b,1,true)<<endl;
+    return 0;
+}
