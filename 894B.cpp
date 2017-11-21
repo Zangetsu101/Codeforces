8c8d1b9251535206a884b06c37032e12406a0975
Zangetsu101 <haidertameem@gmail.com>
2017-11-21 22:59:52 +0600

    Create 894B.cpp

---
+++ 894B.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+#define endl "\n"
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b)
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll modPower(ll a,ll b)
+{
+	ll res=1;
+	while(b)
+	{
+		if(b&1)
+			res=(res*a)%mod;
+		a=(a*a)%mod;
+		b>>=1;
+	}
+	return res;
+}
+
+int main()
+{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+	ll r,c;
+	cin>>r>>c;
+	int k;
+	cin>>k;
+	ll v=modPower(2,r-1);
+	ll ans=modPower(v,c-1);
+	if(k==1)
+		cout<<ans<<endl;
+	else
+	{
+		if((r%2)==(c%2))
+			cout<<ans<<endl;
+		else
+			cout<<0<<endl;
+	}
+	return 0;
+}
