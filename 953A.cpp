7308b6541786d1dcd60f6f4e1eee2bf13eb8b9a5
Zangetsu101 <haidertameem@gmail.com>
2018-03-18 21:16:33 +0600

    Create 953A.cpp

---
+++ 953A.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+#define endl "\n"
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b)
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int func(ll lim)
+{
+	set<ll> s;
+	s.insert(1);
+	int cnt=0;
+	while(!s.empty()&&*s.begin()<=lim)
+	{
+		ll t=*s.begin();
+		s.insert(2*t),s.insert(3*t);
+		s.erase(s.begin());
+		cnt++;
+	}
+	return cnt;
+}
+int main()
+{
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+	ll l,r,ans=0;
+	cin>>l>>r;
+	cout<<func(r)-func(l-1)<<endl;
+	return 0;
+}
