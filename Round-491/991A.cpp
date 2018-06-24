b18db404d5aee01fe7bd2b28d06aa9b5737ed49c
Zangetsu101 <haidertameem@gmail.com>
2018-06-24 19:36:00 +0600

    Create 991A.cpp

---
+++ Round-491/991A.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+#define endl "\n"
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b)
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int power(int a,int b)
+{
+    int ans=1;
+    while(b)
+    {
+        if(b&1)
+            ans*=a;
+        a*=a;
+        b>>=1;
+    }
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    freopen("Input.txt","r",stdin);
+//    freopen("Output.txt","w",stdout);
+    int a,b,c,n;
+    cin>>a>>b>>c>>n;
+    if(n<a||n<b||n<c||a<c||b<c)
+    {
+        cout<<-1<<endl;
+        return 0;
+    }
+    if(n-(a+b-c)>0)
+        cout<<n-(a+b-c)<<endl;
+    else
+        cout<<-1<<endl;
+    return 0;
+}
