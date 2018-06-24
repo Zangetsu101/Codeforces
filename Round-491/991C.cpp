04014dbaa1dd013502320e4eec9d4cac83859472
Zangetsu101 <haidertameem@gmail.com>
2018-06-24 19:36:36 +0600

    Create 991C.cpp

---
+++ Round-491/991C.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <bitset>
+
+#define endl "\n"
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d %d",&a,&b)
+#define siii(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sl(a) scanf("%lld",&a)
+#define sll(a,b) scanf("%lld %lld",&a,&b)
+#define pf printf
+#define vi vector<int>
+#define vii vector<pair<int,int> >
+#define pii pair<int,int>
+#define all(c) c.begin(),c.end()
+#define ms(a,c) memset(a,c,sizeof(a))
+#define tr(c,i) for(decltype((c).begin()) i=(c).begin();i!=(c).end();i++)
+#define trr(c,i) for(decltype((c).rbegin()) i=(c).rbegin();i!=(c).rend();i++)
+
+using namespace std;
+
+#define N 100001
+#define mod 1000000007
+#define inf 1e9
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll n;
+
+bool func(ll k)
+{
+    ll cnt=0;
+    ll temp=n;
+    while(temp>0)
+    {
+        if(temp>=k)
+            cnt+=k,temp-=k;
+        else
+            cnt+=temp,temp=0;
+        if(temp)
+            temp-=(temp/10);
+    }
+    return cnt>=(n+1)/2;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	//freopen("Input.txt","r",stdin); 
+	//freopen("Output.txt","w",stdout);
+    cin>>n;
+    ll low=1,high=n;
+    ll mid;
+    while(low<high)
+    {
+        mid=(low+high)/2;
+        if(func(mid))
+            high=mid;
+        else
+            low=mid+1;
+    }
+    cout<<low<<endl;
+	return 0;
+}
+		
